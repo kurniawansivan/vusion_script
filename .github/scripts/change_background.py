07a83cd0062a8c7a3cc5756bef56666216998434
Ivan Kurniawan S <kurniawansivan@gmail.com>
2024-08-29 16:44:07 +0800

    add local picture in github

--- .github/scripts/change_background.py
+++ .github/scripts/change_background.py
@@ -24,7 +24,7 @@ data = {
     "groupColor": "#FFCC00",
     "layers": [
         {
-            "id": "layer-0",
+            "id": "layer-1",
             "type": "image",  # Bisa diganti sesuai kebutuhan (e.g., video)
             "width": 1920,
             "height": 158,
